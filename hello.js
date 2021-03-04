5dd726e4702de9643e1ea93e8804fcd57cffec96
daniaabughoush <ms1dodo@gmail.com>
2021-03-04 15:47:08 -0800

    new

---
+++ hello.js
@@ -0,0 +1,17 @@
+alert ("welcom to ULTC hope you will have fun during you courses please enter ")
+ var welcom = prompt("please enter hello");
+
+ var txt;
+var r = confirm("Press a button!");
+if (r == true) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed Cancel!";
+}
+  
+
+  
+
+ 
+    
+
